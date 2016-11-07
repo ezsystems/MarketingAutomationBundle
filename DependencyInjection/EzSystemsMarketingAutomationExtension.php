920adbe50d24aaeabebff04076a56ed429bfecc2
Rafał Toborek <kontakt@toborek.info>
2016-11-07 15:07:22 +0100

    Removed unused variables

--- DependencyInjection/EzSystemsMarketingAutomationExtension.php
+++ DependencyInjection/EzSystemsMarketingAutomationExtension.php
@@ -20,7 +20,7 @@ class EzSystemsMarketingAutomationExtension extends Extension implements Prepend
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = $this->getConfiguration($configs, $container);
-        $config = $this->processConfiguration($configuration, $configs);
+        $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
